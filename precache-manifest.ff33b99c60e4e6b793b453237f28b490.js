b32a085908ef2305f6269a4ba345698ca9b0ade7
thuan <maithuan06091997@gmail.com>
2021-11-16 10:31:01 +0700

    Updates

---
+++ precache-manifest.ff33b99c60e4e6b793b453237f28b490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a550043d38600723df79f3dac866b156",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac7d608e06574a8e97f8",
+    "url": "./static/js/2.0d0567b5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0d0567b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de70fe27eb5ea5a8e2fe",
+    "url": "./static/js/main.ccf92b19.chunk.js"
+  },
+  {
+    "revision": "1974c8f12aff3af1269d",
+    "url": "./static/js/runtime-main.78094656.js"
+  }
+]);
